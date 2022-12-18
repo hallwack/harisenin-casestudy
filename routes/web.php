49f7db53dcfbff032b01ebb05a3145a43e738cd8
Raihan Adam <hallwack.id@gmail.com>
2022-12-19 02:59:05 +0700

    feat: add product and categories routes (admin)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.home');
 });
+
+Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryController::class);
 
 Route::get('/iyh', function () {
     return view('mang');
