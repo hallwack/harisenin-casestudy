113325a3e358c26790c53a4075881d50a212c7c5
Raihan Adam <hallwack.id@gmail.com>
2022-12-18 17:46:20 +0700

    feat: add columns in assets table

--- database/migrations/2022_12_18_094419_create_assets_table.php
+++ database/migrations/2022_12_18_094419_create_assets_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("path");
+            $table->integer("size");
             $table->timestamps();
         });
     }
