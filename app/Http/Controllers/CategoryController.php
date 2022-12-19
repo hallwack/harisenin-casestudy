23e9d508f3b105a81623a1c11b1547fc825c72cc
Raihan Adam <hallwack.id@gmail.com>
2022-12-19 19:02:29 +0700

    feat: add store method on CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Asset;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
@@ -14,7 +17,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        return view('pages.admin.category.index');
     }
 
     /**
@@ -24,7 +27,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.category.create');
     }
 
     /**
@@ -35,7 +38,27 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $item = $request->validate([
+            'name' => 'required',
+            'image' => 'required|mimes:jpg,bmp,png'
+        ]);
+
+        $item['image'] = $request->file('image');
+
+        $assets_store = Asset::create([
+            'name' => $item['image']->getFilename() . '.' . $item['image']->getClientOriginalExtension(),
+            'path' => $item['image']->getPathname(),
+            'size' => $item['image']->getSize()
+        ]);
+
+        /* dd($assets_store->id); */
+        $item['category_name'] = $slug = $request->name;
+        $item['category_slug'] = Str::slug($slug);
+        $item['asset_id'] = $assets_store->id;
+
+        Category::create($item);
+
+        return view('pages.admin.category.index');
     }
 
     /**
