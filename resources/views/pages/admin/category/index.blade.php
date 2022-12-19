718cedaa77bf625106d8d4ef34759648821090dc
Raihan Adam <hallwack.id@gmail.com>
2022-12-19 19:01:07 +0700

    fix: change button link

--- resources/views/pages/admin/category/index.blade.php
+++ resources/views/pages/admin/category/index.blade.php
@@ -4,8 +4,11 @@
 
 @section('content')
 <div class="py-12 container mx-auto gap-8 flex flex-col">
-    <h1 class="text-3xl font-semibold">Category</h1>
-    <div class="overflow-x-auto">
+    <div class="flex justify-between">
+        <h1 class="text-3xl font-semibold">Category</h1>
+        <a href="{{ route('categories.create') }}" class="btn">Create</a>
+    </div>
+    <div class="overflow-x-auto h-full">
         <table class="table w-full">
             <!-- head -->
             <thead>
@@ -26,7 +29,7 @@
                     <td><img src="https://placeimg.com/400/225/arch" class="object-cover w-48 rounded-md" alt=""></td>
                     <td>
                         <div class="dropdown">
-                            <label tabindex="0" class="btn m-1">Click</label>
+                            <label tabindex="0" class="btn">Click</label>
                             <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
                                 <li><a>Edit</a></li>
                                 <li><a>Delete</a></li>
